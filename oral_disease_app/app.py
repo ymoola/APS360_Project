3622446053d7f2f8e60f134917bc93c41648f8c0
ymoola <ymoola21@gmail.com>
2025-07-24 22:41:36 -0400

    final change

--- oral_disease_app/app.py
+++ oral_disease_app/app.py
@@ -11,7 +11,7 @@ uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if st.button("Classify"):
         with st.spinner("Analyzing..."):
